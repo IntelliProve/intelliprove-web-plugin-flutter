3bfb52b381ce457efc387757d35ee94bcb95e0ea
Dries Vanmeert <dries.vanmeert@gmail.com>
2024-01-17 17:48:16 +0100

    Add NativeModule for Web Browser * Registered a FlutterMethodChannel called `com.intelliprove/openWebview` * This exposes a single function `openWebView` to which a urlString can be passed from within the Flutter app code * This function then creates a `IntelliWebView` instance through a factory which can be exposed to Swift code. Then it presents this modally on top of the current window's rootViewController, so on top of the Flutter application * This `IntelliWebView` is a SwiftUI View wrapping a WKWebView * Also added the needed code to the WebView's WKUIDelegate to handle any requests for camera or microphone usage, so that preferences are stored on the device * Otherwise, WKWebView prompts the user for permission every time it needs access

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -3,11 +3,36 @@ import Flutter
 
 @UIApplicationMain
 @objc class AppDelegate: FlutterAppDelegate {
-  override func application(
-    _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
-  ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
-    return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-  }
+    override func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+    ) -> Bool {
+        GeneratedPluginRegistrant.register(with: self)
+
+        let controller : FlutterViewController = window?.rootViewController as! FlutterViewController
+        let webviewChannel = FlutterMethodChannel(
+            name: "com.intelliprove/openWebview",
+            binaryMessenger: controller.binaryMessenger
+        )
+
+        webviewChannel.setMethodCallHandler({ [weak self] (call: FlutterMethodCall, result: FlutterResult) -> Void in
+            if call.method == "openWebview" {
+                if let args = call.arguments as? [String: Any],
+                   let urlString = args["url"] as? String {
+                    self?.openWebView(urlString: urlString)
+                }
+                result(nil)
+            } else {
+                result(FlutterMethodNotImplemented)
+            }
+        })
+
+        return super.application(application, didFinishLaunchingWithOptions: launchOptions)
+    }
+
+    private func openWebView(urlString: String) {
+        let webViewController = IntelliWebViewFactory.newWebView(urlString: urlString)
+        webViewController.modalPresentationStyle = .fullScreen
+        window?.rootViewController?.present(webViewController, animated: true)
+    }
 }
