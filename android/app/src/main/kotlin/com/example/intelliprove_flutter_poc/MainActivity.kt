4b4e009fc1ffc1bb4dc9b38a767dc2049d4b9d66
Dries Vanmeert <dries.vanmeert@gmail.com>
2024-02-21 16:36:53 +0100

    * Added IntelliProveSDK for Android * Add needed permissions and the IntelliWebViewActivity to the manifest * Using MethodChannel, just like on iOS, to start the WebView as a new Activity on top of the Flutter App * PostMessages received from the SDK are emitted to Flutter

--- android/app/src/main/kotlin/com/example/intelliprove_flutter_poc/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/intelliprove_flutter_poc/MainActivity.kt
@@ -1,6 +1,41 @@
 package com.example.intelliprove_flutter_poc
 
+import com.intelliprove.webview.IntelliWebViewActivity
+import com.intelliprove.webview.IntelliWebViewDelegate
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity(private var webViewChannel: MethodChannel? = null): FlutterActivity(), IntelliWebViewDelegate {
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        webViewChannel = MethodChannel(
+            flutterEngine.dartExecutor.binaryMessenger,
+            "com.intelliprove/webview"
+        )
+
+        webViewChannel?.setMethodCallHandler { call, result ->
+            if (call.method == "openWebview") {
+                val args = call.arguments as? Map<*, *>
+                val urlString = args?.get("url") as? String
+                urlString?.let {
+                    openWebView(it)
+                }
+                result.success(null)
+            } else {
+                result.notImplemented()
+            }
+        }
+    }
+
+    private fun openWebView(urlString: String) {
+        IntelliWebViewActivity.start(this, urlString, this)
+    }
+
+    override fun didReceivePostMessage(postMessage: String) {
+        runOnUiThread {
+            webViewChannel?.invokeMethod("didReceivePostMessage", postMessage)
+        }
+    }
 }
