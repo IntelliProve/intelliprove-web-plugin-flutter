d3f6d9a5dca5a118d2423f1c22b8dbecb9a18143
Tanguy <tanguy.sanglet@intelliprove.com>
2025-09-14 21:22:50 +0200

    ⚙️ iOS config changes

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -2,7 +2,7 @@ import UIKit
 import Flutter
 import IntelliProveSDK
 
-@UIApplicationMain
+@main
 @objc class AppDelegate: FlutterAppDelegate {
     override func application(
         _ application: UIApplication,
